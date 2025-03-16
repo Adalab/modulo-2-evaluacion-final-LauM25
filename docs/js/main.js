6ee63dccbb7989454bd39fc1f1e58f4279c0335b
LauM25 <laurammwork93@gmail.com>
2025-03-16 22:42:15 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,22 @@
-console.log(">> Ready for the second exam :) / :(");const l=document.querySelector(".js-btnSearch"),i=document.querySelector(".js-btnReset"),a=document.querySelector(".js-input"),c=document.querySelector(".js-sectionResult");document.querySelector(".js-sectionFav");function s(t){c.innerHTML="";for(const e of t)c.innerHTML+=`<div class="container-info " id="${e.mal_id}">
+console.log(">> Ready for the second exam :) / :(");const m=document.querySelector(".js-btnSearch"),d=document.querySelector(".js-btnReset"),g=document.querySelector(".js-input"),l=document.querySelector(".js-sectionResult"),c=document.querySelector(".js-sectionFav");let o=[],s=[];const a=JSON.parse(localStorage.getItem("favSeries"));if(a!==null){o=a,c.innerHTML="";for(const e of a){let t="";t+=`
+        <div class="container-info" id="${e.mal_id}">
         <img class="imgResult" src="${e.images.jpg.image_url}" alt:"Imagen serie anime"></img>
         <p>Titulo: ${e.title}</p>
-        </div>`}const n=JSON.parse(localStorage.getItem("series"));function r(t){console.log("Esto es el localStorage"+n),n!==null?s(n):fetch(`https://api.jikan.moe/v4/anime?q=${t}`).then(e=>e.json()).then(e=>{const o=e.data;localStorage.setItem("series",JSON.stringify(o)),s(o)})}function u(t){t.preventDefault(),console.log("Ha hecho click");const e=a.value;r(e)}l.addEventListener("click",u);function d(t){t.preventDefault(),localStorage.clear(t)}i.addEventListener("click",d);
+        </div>
+        `,c.innerHTML+=t}}function u(e){const t=parseInt(e.currentTarget.id),n=s.find(i=>t===i.mal_id);o.push(n),c.innerHTML="";for(const i of o){let r="";r+=`
+            <div class="container-info" id="${i.mal_id}">
+            <img class="imgResult" src="${i.images.jpg.image_url}" alt:"Imagen serie anime"></img>
+            <p>Titulo: ${i.title}</p>
+            </div>
+            `,c.innerHTML+=r}localStorage.setItem("favSeries",JSON.stringify(o))}function f(e){l.innerHTML="",console.log(e);for(const n of e)if(n.images.jpg.image_url==="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"){let i="";i+=`
+        <div class="container-info" id="${n.mal_id}">
+        <img class="imgResult" src="https://icon-library.com/images/image-placeholder-icon/image-placeholder-icon-3.jpg" alt:"Imagen serie anime"></img>
+        <p>Titulo: ${n.title}</p>
+        </div>
+        `,l.innerHTML+=i}else{let i="";i+=`
+        <div class="container-info" id="${n.mal_id}">
+        <img class="imgResult" src="${n.images.jpg.image_url}" alt:"Imagen serie anime"></img>
+        <p>Titulo: ${n.title}</p>
+        </div>
+        `,l.innerHTML+=i}const t=document.querySelectorAll(".container-info");for(const n of t)n.addEventListener("click",u)}function p(e){fetch(`https://api.jikan.moe/v4/anime?q=${e}`).then(t=>t.json()).then(t=>{s=t.data,f(s)})}function v(e){e.preventDefault(),console.log("Ha hecho click");const t=g.value;p(t)}m.addEventListener("click",v);function S(e){e.preventDefault(),localStorage.clear(e)}d.addEventListener("click",S);
 //# sourceMappingURL=main.js.map
